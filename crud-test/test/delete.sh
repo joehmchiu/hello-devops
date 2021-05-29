e25028996b242656b2bafa1748cfc871d21e5e09
Your Name <you@example.com>
2021-05-29 20:01:01 +1000

    init

--- crud-test/test/delete.sh
+++ crud-test/test/delete.sh
@@ -5,4 +5,4 @@ ID=$2
 [ -z $URL ] && { echo "Error: URL not found"; exit 3; }
 [ -z $ID ] && { echo "Error: ID not found"; exit 3; }
 
-curl -X DELETE "http://$1/api/v1/users/$2"
+curl -s -X DELETE "$1$2"
